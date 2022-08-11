d8810d751659d278f8ebc3505862927bc506a217
abdullahayhan <aayhaan4@gmail.com>
2022-08-11 19:37:31 +0300

    Proje dosyası ekle.

---
+++ measuringTemperature/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿
+namespace measuringTemperature
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_Temperature = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(6, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(159, 34);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Temperature : ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.UseCompatibleTextRendering = true;
+            // 
+            // lbl_Temperature
+            // 
+            this.lbl_Temperature.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbl_Temperature.Location = new System.Drawing.Point(171, 28);
+            this.lbl_Temperature.Name = "lbl_Temperature";
+            this.lbl_Temperature.Size = new System.Drawing.Size(77, 28);
+            this.lbl_Temperature.TabIndex = 1;
+            this.lbl_Temperature.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.lbl_Temperature.UseCompatibleTextRendering = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(260, 93);
+            this.Controls.Add(this.lbl_Temperature);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Measuring Temperature";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_Temperature;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ measuringTemperature/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+
+namespace measuringTemperature
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(@"root\WMI", "SELECT * FROM MSAcpi_ThermalZoneTemperature");
+
+            foreach (ManagementObject obj in searcher.Get())
+            {
+               Double temperature = Convert.ToDouble(obj["CurrentTemperature"].ToString());
+                temperature = (temperature / 10 -273);
+                lbl_Temperature.Text = temperature.ToString();
+            }
+           
+        }
+    }
+}
